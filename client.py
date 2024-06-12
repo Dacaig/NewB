39fed403475daa8cb31d6b9681f5fe9561f69a54
Dacaig <137755474+Dacaig@users.noreply.github.com>
2024-06-12 19:21:52 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,68 +1,108 @@
 from collections import OrderedDict
-from typing import Dict, List, Tuple
-
+from typing import List, Tuple
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from datasets.utils.logging import disable_progress_bar
+from torch.utils.data import DataLoader
 
-import cifar
 import flwr as fl
+from flwr.common import Metrics
+from flwr_datasets import FederatedDataset
 
-DEVICE: str = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE: str = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+NUM_CLIENTS=5
+BATCH_SIZE=32
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-class CifarClient(fl.client.NumPyClient):
-    """Flower client implementing CIFAR-10 image classification using
-    PyTorch."""
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-    def __init__(
-        self,
-        model: cifar.Net,
-        trainloader: torch.utils.data.DataLoader,
-        testloader: torch.utils.data.DataLoader,
-        num_examples: Dict,
-    ) -> None:
-        self.model = model
-        self.trainloader = trainloader
-        self.testloader = testloader
-        self.num_examples = num_examples
-
-    def get_parameters(self, config) -> List[np.ndarray]:
-        # Return model parameters as a list of NumPy ndarrays
-        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
-
-    def set_parameters(self, parameters: List[np.ndarray]) -> None:
-        # Set model parameters from a list of NumPy ndarrays
-        params_dict = zip(self.model.state_dict().keys(), parameters)
-        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-        self.model.load_state_dict(state_dict, strict=True)
-
-    def fit(
-        self, parameters: List[np.ndarray], config: Dict[str, str]
-    ) -> Tuple[List[np.ndarray], int, Dict]:
-        # Set model parameters, train model, return updated model parameters
-        self.set_parameters(parameters)
-        cifar.train(self.model, self.trainloader, epochs=1, device=DEVICE)
-        return self.get_parameters(config={}), self.num_examples["trainset"], {}
-
-    def evaluate(
-        self, parameters: List[np.ndarray], config: Dict[str, str]
-    ) -> Tuple[float, int, Dict]:
-        # Set model parameters, evaluate model on local test dataset, return result
-        self.set_parameters(parameters)
-        loss, accuracy = cifar.test(self.model, self.testloader, device=DEVICE)
-        return float(loss), self.num_examples["testset"], {"accuracy": float(accuracy)}
-
-def main() -> None:
-    """Load data, start CifarClient."""
-
-    # Load model and data
-    model = cifar.Net()
-    model.to(DEVICE)
-    trainloader, testloader, num_examples = cifar.load_data()
-
-    # Start client
-    client = CifarClient(model, trainloader, testloader, num_examples)
-    fl.client.start_client(server_address="0.0.0.0:8080", client = client.to_client())
+def train(net, trainloader, epochs: int, verbose=False):
+    """Train the network on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters())
+    net.train()
+    for epoch in range(epochs):
+        correct, total, epoch_loss = 0, 0, 0.0
+        for batch in trainloader:
+            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # Metrics
+            epoch_loss += loss
+            total += labels.size(0)
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+        epoch_loss /= len(trainloader.dataset)
+        epoch_acc = correct / total
+        if verbose:
+            print(f"Epoch {epoch+1}: train loss {epoch_loss}, accuracy {epoch_acc}")
 
 
-if __name__ == "__main__":
-    main()
+def test(net, testloader):
+    """Evaluate the network on the entire test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for batch in testloader:
+            images, labels = batch["img"].to(DEVICE), batch["label"].to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    loss /= len(testloader.dataset)
+    accuracy = correct / total
+    return loss, accuracy
+
+def load_datasets(partition_id):
+    fds = FederatedDataset(dataset="cifar10", partitioners={"train": NUM_CLIENTS})
+    partition=fds.load_split(partition_id)
+    partition_train_test=partition.train_test_split(test_size=0.2)
+    pytorch_transforms=transforms.Compose(
+        [transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    def apply_transforms(batch):
+        # Instead of passing transforms to CIFAR10(..., transform=transform)
+        # we will use this function to dataset.with_transform(apply_transforms)
+        # The transforms object is exactly the same
+        batch["img"] = [pytorch_transforms(img) for img in batch["img"]]
+        return batch
+
+partition_train_test=partition_train_test.with_transform(apply_transforms)
+
+    # Create train/val for each partition and wrap it into DataLoader
+    trainloaders = []
+    valloaders = []
+    for partition_id in range(NUM_CLIENTS):
+        partition = fds.load_partition(partition_id, "train")
+        partition = partition.with_transform(apply_transforms)
+        partition = partition.train_test_split(train_size=0.8, seed=42)
+        trainloaders.append(DataLoader(partition["train"], batch_size=BATCH_SIZE))
+        valloaders.append(DataLoader(partition["test"], batch_size=BATCH_SIZE))
+    testset = fds.load_split("test").with_transform(apply_transforms)
+    testloader = DataLoader(testset, batch_size=BATCH_SIZE, num_workers=0)
+    return trainloaders, valloaders, testloader
