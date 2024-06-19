c9f46b701b7ae69a8b883250520879083df80a34
Dacaig <137755474+Dacaig@users.noreply.github.com>
2024-06-19 14:30:29 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -116,16 +116,6 @@ trainloader,testloader=load_datasets(partition_id=id)
 
 
 
-def set_parameters(net, parameters: List[np.ndarray]):
-    params_dict = zip(net.state_dict().keys(), parameters)
-    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
-    net.load_state_dict(state_dict, strict=True)
-
-
-def get_parameters(net) -> List[np.ndarray]:
-    return [val.cpu().numpy() for _, val in net.state_dict().items()]
-
-
 class FlowerClient(fl.client.NumPyClient):
     def set_parameters(net, parameters: List[np.ndarray]):
         params_dict = zip(net.state_dict().keys(), parameters)
